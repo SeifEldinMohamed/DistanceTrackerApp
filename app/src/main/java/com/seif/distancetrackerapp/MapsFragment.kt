963c24f5c057884e0bcd4587217e9cc90f5dd037
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-07-04 14:22:22 +0200

    Enable my location layer

--- app/src/main/java/com/seif/distancetrackerapp/MapsFragment.kt
+++ app/src/main/java/com/seif/distancetrackerapp/MapsFragment.kt
@@ -1,11 +1,13 @@
 package com.seif.distancetrackerapp
 
+import android.annotation.SuppressLint
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -14,10 +16,15 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.seif.distancetrackerapp.databinding.FragmentMapsBinding
+import com.seif.distancetrackerapp.util.ExtensionsFunctions.hide
+import com.seif.distancetrackerapp.util.ExtensionsFunctions.show
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-class MapsFragment : Fragment(), OnMapReadyCallback  {
+class MapsFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener  {
     private var _binding: FragmentMapsBinding? = null
     private val binding get() = _binding!!
+    lateinit var map: GoogleMap
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -42,15 +49,35 @@ class MapsFragment : Fragment(), OnMapReadyCallback  {
 
         }
     }
-    override fun onMapReady(map: GoogleMap) {
-        val sydney = LatLng(-34.0, 151.0)
-        map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    @SuppressLint("MissingPermission")
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        map.isMyLocationEnabled = true
+        map.setOnMyLocationButtonClickListener(this)
+
+        map.uiSettings.apply {
+            isZoomControlsEnabled = false
+            isZoomGesturesEnabled = false
+            isTiltGesturesEnabled = false
+            isScrollGesturesEnabled = false
+            isRotateGesturesEnabled = false
+            isCompassEnabled = false
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onMyLocationButtonClick(): Boolean {
+        binding.txtHint.animate().alpha(0f).duration = 1500
+        lifecycleScope.launch {
+            delay(2500L)
+            binding.txtHint.hide()
+            binding.btnStart.show()
+        }
+        return false
     }
 
 }
---
+++ app/src/main/java/com/seif/distancetrackerapp/util/ExtensionsFunctions.kt
@@ -0,0 +1,19 @@
+package com.seif.distancetrackerapp.util
+
+import android.view.View
+import android.widget.Button
+
+object ExtensionsFunctions {
+    fun View.show(){
+        this.visibility = View.VISIBLE
+    }
+    fun View.hide(){
+        this.visibility = View.INVISIBLE
+    }
+    fun Button.enable(){
+        this.isEnabled = true
+    }
+    fun Button.disable(){
+        this.isEnabled = false
+    }
+}
