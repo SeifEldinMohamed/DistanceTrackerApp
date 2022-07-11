5b77dbd862eb6283fac55c6fa2285c1ca903fd1b
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-07-11 20:06:06 +0200

    Design ResultFragment Layout

--- app/src/main/java/com/seif/distancetrackerapp/ui/result/ResultFragment.kt
+++ app/src/main/java/com/seif/distancetrackerapp/ui/result/ResultFragment.kt
@@ -5,16 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.seif.distancetrackerapp.R
+import com.seif.distancetrackerapp.databinding.FragmentResultBinding
 
-class ResultFragment : Fragment() {
+class ResultFragment : BottomSheetDialogFragment() {
 
+    private var _binding: FragmentResultBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_result, container, false)
+        _binding = FragmentResultBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
